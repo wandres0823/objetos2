69bb1ae09a2d9f21909b39c3d9e2c79f44304173
“william” <william200928@gmail.com>
2016-09-11 05:32:43 +0200

    terminada

---
+++ src/clase/DenominadorCeroException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase;
+
+/**
+ *
+ * @author adalberto
+ */
+public class DenominadorCeroException extends Exception {
+    public DenominadorCeroException(){
+        super ("no se permite denominador cero");
+        
+    }
+}
---
+++ src/clase/Mixto.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase;
+
+/**
+ *
+ * @author adalberto
+ */
+public class Mixto {
+    private int entero;
+    private int numerador;
+    private int denominador;
+    
+    
+    public Mixto (int entero, int numerador, int denominador) throws DenominadorCeroException{
+        this.entero = entero;
+        this.numerador= numerador;
+        this.denominador= denominador;
+         if(denominador ==0){
+             throw new DenominadorCeroException();
+         }
+    }
+
+    public int getEntero() {
+        return entero;
+    }
+
+    public int getNumerador() {
+        return numerador;
+    }
+
+    public int getDenominador() {
+        return denominador;
+    }
+
+    public void setEntero(int entero) {
+        this.entero = entero;
+    }
+
+    public void setNumerador(int numerador) {
+        this.numerador = numerador;
+    }
+
+    public void setDenominador(int denominador) {
+        this.denominador = denominador;
+    }
+    public Mixto sumar(Mixto f2) throws DenominadorCeroException {
+    Mixto f;
+    int enter,nume, den,nfra1,nfra2,numeres,deres;    
+        
+    nfra1= (this.entero * this.denominador) + this.numerador;
+    nfra2= (f2.entero * f2.denominador) + f2.numerador;
+    
+    numeres= (nfra1 * f2.denominador) + (this.denominador * nfra2);
+    deres= this.denominador * f2.denominador;
+    
+    enter= numeres / deres;
+    nume= numeres % deres;
+    den= deres;
+    
+    f = new Mixto(enter,nume, den);
+    return f;
+    }
+     public Mixto resta (Mixto f2) throws DenominadorCeroException {
+    Mixto f;
+    int enter,nume, den,nfra1,nfra2,numeres,deres;    
+        
+    nfra1= (this.entero * this.denominador) + this.numerador;
+    nfra2= (f2.entero * f2.denominador) + f2.numerador;
+    
+    numeres= (nfra1 * f2.denominador) -(this.denominador * nfra2);
+    deres= this.denominador * f2.denominador;
+    
+    enter= numeres / deres;
+    nume= numeres % deres;
+    den= deres;
+    
+    f = new Mixto(enter,nume, den);
+    return f;
+    }
+     
+      public Mixto multiplicacion (Mixto f2) throws DenominadorCeroException {
+    Mixto f;
+    int enter,nume, den,nfra1,nfra2,numeres,deres;    
+        
+    nfra1= (this.entero * this.denominador) + this.numerador;
+    nfra2= (f2.entero * f2.denominador) + f2.numerador;
+    
+    numeres= nfra1 * nfra2;
+    deres= this.denominador * f2.denominador;
+    
+    enter= numeres / deres;
+    nume= numeres % deres;
+    den= deres;
+    
+    f = new Mixto(enter,nume, den);
+    return f;
+    }
+      public Mixto dividir (Mixto f2) throws DenominadorCeroException {
+    Mixto f;
+    int ent,num, den,nfrac1,nfrac2,nres,dres;    
+        
+    nfrac1= (this.entero * this.denominador) + this.numerador;
+    nfrac2= (f2.entero * f2.denominador) + f2.numerador;
+    
+    nres= nfrac1 * f2.denominador;
+    dres= this.denominador * nfrac2;
+    
+    ent= nres / dres;
+    num= nres % dres;
+    den= dres;
+    
+     f = new Mixto(ent,num, den);
+    return f;
+    }
+}
